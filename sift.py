86fd4a3515ab68d3f6f0d27b140cdc64b2bb9694
omar shaalan <omarshaalan31@gmail.com>
2019-03-28 23:14:28 +0200

    .

--- sift.py
+++ sift.py
@@ -5,12 +5,17 @@ def getKeypoints(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     sift = cv2.xfeatures2d.SIFT_create()
     kp, des = sift.detectAndCompute(gray, None)
-    img = cv2.drawKeypoints(gray, kp, outImage=np.array([]))
-    cv2.imwrite("new.png", img)
     key_points= np.zeros((len(kp),2))
     for i in range(len(kp)):
         key_points[i,0] = kp[i].angle
         key_points[i, 1] = kp[i].octave
     return key_points,des
+
+
+def getDes(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp, des = sift.detectAndCompute(gray, None)
+    return des
 
 
