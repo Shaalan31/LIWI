5f7285ff5bb366f42a3172d598fb5e01b870da6f
omar shaalan <omarshaalan31@gmail.com>
2019-04-01 14:45:42 +0200

    DPR done

--- DataPreprocessor.py
+++ DataPreprocessor.py
@@ -10,8 +10,9 @@ def data_preprocessor():
     SDpoints = np.zeros((1,128))
     for filename in glob.glob('WordsDatabase/*/*/*.png'):
         SDpoints = np.append(SDpoints,sift.getDes(cv.imread(filename)),axis=0)
+    SDpoints = np.delete(SDpoints,0,0)
+    SDpoints, _,_ = common.feature_normalize(SDpoints)
     with h5py.File('SDpoints.h5', 'w') as hf:
         hf.create_dataset("keypoints-of-Iam", data=SDpoints)
 
 
-data_preprocessor()
