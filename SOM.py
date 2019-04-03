c1cf8ae0c4a301c6ba73d8328bbde047b74a7308
omar shaalan <omarshaalan31@gmail.com>
2019-04-03 23:24:29 +0200

    ready for training

--- SOM.py
+++ SOM.py
@@ -1,32 +1,29 @@
 import numpy as np
-from neupy import algorithms, utils
+from neupy import algorithms, utils, storage
 import h5py
 
 
 def on_epoch_end(optimizer):
     print("Last epoch: {}".format(optimizer.last_epoch))
+    storage.load(optimizer, filepath='file.hdf5')
 
-def codebook_generation():
+def codebook_generation(num_batches,sofm,epoch):
 
-    sofm = algorithms.SOFM(n_inputs = 128,
-                                 n_outputs = 300 ,
-                                 step = 0.5,
-                                 learning_radius = 0,
-                                 signals=on_epoch_end
-                                 )
+    if(sofm is None):
+        sofm = algorithms.SOFM(n_inputs = 128,
+                                     n_outputs = 300 ,
+                                     step = 0.5,
+                                     learning_radius = 0,
+                                     signals=on_epoch_end
+                                     )
 
-    with h5py.File('Datasets/SDpoints1.h5', 'r') as hf:
+
+    with h5py.File('Datasets/SDpoints0.h5', 'r') as hf:
         data = hf['keypoints-batch'][:]
-
-    print(data.shape)
-    #print(sofm.weight)
-    #sofm.init_weights(data)
-
-    #for x in range(1000):
-    sofm.train(data,epochs=5)
-    #print(sofm.predict(np.array([2])))
-    print(sofm.weight.shape)
-    #sofm.train(data,epoch=1)
+    for x in range(1,int(num_batches)+1):
+        with h5py.File('Datasets/SDpoints0.h5', 'r') as hf:
+            data = np.append(data,hf['keypoints-batch'][:],axis=0)
+    sofm.train(data,epochs=int(epoch))
 
 
-codebook_generation()
+codebook_generation(12,None,5)
