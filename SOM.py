7494924bdd5a555fe174ec23b6a4912727d6e07a
omar shaalan <omarshaalan31@gmail.com>
2019-04-03 23:10:29 +0200

    .

--- SOM.py
+++ SOM.py
@@ -8,27 +8,24 @@ def on_epoch_end(optimizer):
 
 def codebook_generation():
 
-    sofm = algorithms.SOFM(n_inputs = 1,
-                                 n_outputs = 4 ,
+    sofm = algorithms.SOFM(n_inputs = 128,
+                                 n_outputs = 300 ,
                                  step = 0.5,
-                                 learning_radius = 0
-                                 #signals=on_epoch_end
+                                 learning_radius = 0,
+                                 signals=on_epoch_end
                                  )
-    data = np.zeros((4,1))
-    data[0] = 0
-    data[1] = 1
-    data[2] = 2
-    data[3] = 3
-    print(data)
-    print(sofm.weight)
+
+    with h5py.File('Datasets/SDpoints1.h5', 'r') as hf:
+        data = hf['keypoints-batch'][:]
+
+    print(data.shape)
+    #print(sofm.weight)
     #sofm.init_weights(data)
 
-    #with h5py.File('SDpoints.h5', 'r') as hf:
-    #    data = hf['keypoints-of-Iam'][:]
     #for x in range(1000):
-    sofm.train(data,epochs=10000)
+    sofm.train(data,epochs=5)
     #print(sofm.predict(np.array([2])))
-    print(sofm.weight)
+    print(sofm.weight.shape)
     #sofm.train(data,epoch=1)
 
 
