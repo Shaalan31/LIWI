679d5c955dc36e8a7006df8edd6704ed9ac4ab1b
omar shaalan <omarshaalan31@gmail.com>
2019-04-03 22:58:01 +0200

    SOM draft

--- SOM.py
+++ SOM.py
@@ -8,15 +8,28 @@ def on_epoch_end(optimizer):
 
 def codebook_generation():
 
-    sofm = algorithms.SOFM(n_inputs = 128,
-                                 n_outputs = 300,
-                                 step = 0.1,
-                                 learning_radius = 0,
-                                 signals=on_epoch_end
+    sofm = algorithms.SOFM(n_inputs = 1,
+                                 n_outputs = 4 ,
+                                 step = 0.5,
+                                 learning_radius = 0
+                                 #signals=on_epoch_end
                                  )
-    with h5py.File('SDpoints.h5', 'r') as hf:
-        data = hf['keypoints-of-Iam'][:]
-    sofm.train(data,epoch=1)
+    data = np.zeros((4,1))
+    data[0] = 0
+    data[1] = 1
+    data[2] = 2
+    data[3] = 3
+    print(data)
+    print(sofm.weight)
+    #sofm.init_weights(data)
+
+    #with h5py.File('SDpoints.h5', 'r') as hf:
+    #    data = hf['keypoints-of-Iam'][:]
+    #for x in range(1000):
+    sofm.train(data,epochs=10000)
+    #print(sofm.predict(np.array([2])))
+    print(sofm.weight)
+    #sofm.train(data,epoch=1)
 
 
 codebook_generation()
--- test.py
+++ test.py
@@ -24,6 +24,53 @@ import h5py
 # print(n,'\n',n.shape)
 #
 
-with h5py.File('SDpoints.h5', 'r') as hf:
-    data = hf["keypoints-batch12"][:]
-print(data.shape)
+# with h5py.File('SDpoints.h5', 'r') as hf:
+#     data = hf["keypoints-batch12"][:]
+# print(data.shape)
+
+
+
+#import numpy as np
+import matplotlib.pyplot as plt
+
+from neupy import algorithms, utils
+
+
+utils.reproducible()
+plt.style.use('ggplot')
+
+
+X = np.array([
+    [0.1961, 0.9806],
+    [-0.1961, 0.9806],
+    [0.9806, 0.1961],
+    [0.9806, -0.1961],
+    [-0.5812, -0.8137],
+    [-0.8137, -0.5812],
+])
+
+sofmnet = algorithms.SOFM(
+    n_inputs=2,
+    n_outputs=3,
+
+    step=0.5,
+    show_epoch=20,
+    shuffle_data=True,
+    verbose=True,
+
+    learning_radius=0,
+    features_grid=(3, 1),
+)
+
+plt.plot(X.T[0:1, :], X.T[1:2, :], 'ko')
+sofmnet.train(X, epochs=100)
+
+print("> Start plotting")
+plt.xlim(-1, 1.2)
+plt.ylim(-1, 1.2)
+
+plt.plot(sofmnet.weight[0:1, :], sofmnet.weight[1:2, :], 'bx')
+print(sofmnet.weight)
+print(sofmnet.features_grid)
+for data in X:
+    print( sofmnet.predict(np.reshape(data, (2, 1)).T))
