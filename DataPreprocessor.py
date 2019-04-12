876b305530bc7f236d04f797eb51a8aed1cc2577
omar shaalan <omarshaalan31@gmail.com>
2019-04-12 16:19:11 +0200

    test

--- DataPreprocessor.py
+++ DataPreprocessor.py
@@ -39,14 +39,30 @@ def data_preprocessor():
 
 
 
-def normalize_batches():
+def normalize():
     SDpoints = np.zeros((1, 128))
+    SDpointsFinal = np.zeros((1, 128))
     for x in range(0,244):
+        print(x)
         with h5py.File('Datasets/SDpoints'+str(x) +'.h5', 'r') as hf:
-            SDpoints = np.append(SDpoints, hf['keypoints-batch'][:])
+            SDpoints = np.append(SDpoints, hf['keypoints-batch'][:],axis=0)
+        if(x % 50 == 0):
+            SDpoints = np.delete(SDpoints, 0, 0)
+            SDpointsFinal = np.append(SDpointsFinal,SDpoints,axis=0)
+            SDpoints = np.zeros((1, 128))
     SDpoints = np.delete(SDpoints, 0, 0)
-    SDpoints, _,_ = common.feature_normalize(SDpoints)
+    SDpointsFinal = np.append(SDpointsFinal, SDpoints,axis=0)
+    SDpoints = np.zeros((1, 128))
+    print(SDpointsFinal.shape)
+    SDpointsFinal = np.delete(SDpoints, 0, 0)
+    SDpointsFinal, mean,dev = common.feature_normalize(SDpointsFinal)
+    with open("stats.txt", 'a') as out:
+        out.write("Mean:  "+str(mean) + " \nDev: " + str(dev))
+    with h5py.File('Datasets/AData.h5', 'w') as hf:
+        hf.create_dataset("keypoints-batch", data=SDpoints)
+
+
 
 #data_preprocessor()
 
-normalize_batches()
+normalize()
