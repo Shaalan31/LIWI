32ff2f1530ff3d572ad189dfd1de8eb38f16df14
omar shaalan <omarshaalan31@gmail.com>
2019-04-30 19:44:40 +0200

    SOM Semi done

--- SOM.py
+++ SOM.py
@@ -1,11 +1,21 @@
 import numpy as np
 from neupy import algorithms, utils, storage
 import h5py
-
+import pickle
 
 def on_epoch_end(optimizer):
     print("Last epoch: {}".format(optimizer.last_epoch))
     storage.load(optimizer, filepath='file.hdf5')
+
+def train_sofm(data,ep=1):
+    with open('sofm.pkl','rb') as input:
+        sofm = pickle.load(input)
+    sofm.train(data,epochs=ep)
+    with open('sofm.pkl','wb') as output:
+        pickle.dump(sofm,output,pickle.HIGHEST_PROTOCOL)
+
+    return sofm
+
 
 def codebook_generation(num_batches,sofm,epoch):
 
@@ -26,4 +36,3 @@ def codebook_generation(num_batches,sofm,epoch):
     sofm.train(data,epochs=int(epoch))
 
 
-codebook_generation(12,None,5)
