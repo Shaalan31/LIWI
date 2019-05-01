c408012c9b0533706d384605cc3916f9161bde5a
Unknown <samar.gamal144@gmail.com>
2019-05-01 23:16:44 +0200

    update testing

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from word_segmentation import *
 from feature_matching import *
 
+
 def tester(name, path):
     image = cv2.imread(name)
     image = remove_shadow(image)
@@ -19,75 +20,84 @@ def tester(name, path):
     sd, so = word_segmentation(image, path)
 
     # calculate SDS and SOH
-    SDS = features.sds(sd, code_book, t=30)
+    SDS = features.sds(sd, code_book, t=1)
     SOH = features.soh(so, phi=36)
-    # print(SDS, '\n', SOH)
+
     return SDS, SOH
 
+
+# Train the model
+def train(first_class, last_class):
+
+    base_train = 'C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/Samples/'
+    
+    count = first_class
+    SDS_train = []
+    SOH_train = []
+    print('Training The Model:')
+    
+    while count <= last_class:
+        for filename in glob.glob(base_train + 'Class' + str(count)):
+            print('Class' + str(count) + ':')
+
+            for image in glob.glob(filename + '/*.png'):
+                name = Path(image).name
+                print(name)
+    
+                SDS, SOH = tester(base_train + 'Class' + str(count) + '/' + name, name)
+                SDS_train.append(SDS)
+                SOH_train.append(SOH)
+    
+            count += 1
+            
+    return SDS_train, SOH_train
+
+
+# Test the model
+def test(first_class, last_class, SDS_train, SOH_train):
+
+    base_test = 'C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/TestCases/'
+    total_test_cases = 0
+    right_test_cases = 0
+    count = first_class
+    print('Testing The Model:')
+    
+    while count <= last_class:
+        print('Class' + str(count) + ':')
+    
+        for filename in glob.glob(base_test + 'testing' + str(count) + '_*.png'):
+            name = Path(filename).name
+    
+            SDS, SOH = tester(base_test + name, name)
+    
+            distances = []
+            for i in range(0, len(SDS_train)):
+                D = match(u=SDS, v=SDS_train[i], x=SOH, y=SOH_train[i], w=0.35)
+                distances.append(D)
+    
+            class_numb = int(np.argmin(distances) / 2) + first_class
+            print(name + ' , class number: ' +  str(class_numb))
+    
+            if(class_numb == count):
+                right_test_cases += 1
+            total_test_cases += 1
+    
+            accuracy = (right_test_cases / total_test_cases) * 100
+            print('Accuracy:  ' + str(accuracy) + '%')
+    
+            # break
+        count += 1
 
 
 # code book
 code_book = pickle.load( open( "centers.pkl", "rb" ) )
-# tester('C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/TestCases/testing1_27.png')
+# tester('C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/TestCases/testing24_2.png', 'testing24_2.png')
 
-
-# Train the model
-base_train = 'C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/Samples/'
-class_count = 20
-count = 1
-SDS_train = []
-SOH_train = []
-print('Training The Model:')
-
-while count <= class_count:
-    for filename in glob.glob(base_train + 'Class' + str(count)):
-        print('Class' + str(count) + ':')
-        image_count = 1
-        for image in glob.glob(filename + '/*.png'):
-            name = Path(image).name
-            print(name)
-
-            SDS, SOH = tester(base_train + 'Class' + str(count) + '/' + name, name)
-            SDS_train.append(SDS)
-            SOH_train.append(SOH)
-
-        count += 1
+SDS, SOH = train(first_class=61, last_class=90)
+test(first_class=61, last_class=90, SDS_train=SDS, SOH_train=SOH)
 
 
 
-# Test the model
-base_test = 'C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/TestCases/'
-total_test_cases = 0
-right_test_cases = 0
-count = 1
-distances = []
-print('Testing The Model:')
-
-while count <= class_count:
-    print('Class' + str(count) + ':')
-
-    for filename in glob.glob(base_test + 'testing' + str(count) + '_*.png'):
-        name = Path(filename).name
-
-        SDS, SOH = tester(base_test + name, name)
-
-        distances = []
-        for i in range(0, len(SDS_train)):
-            D = match(u=SDS, v=SDS_train[i], x=SOH, y=SOH_train[i], w=0.1)
-            distances.append(D)
-
-        class_numb = int(np.argmin(distances) / 2) + 1
-        print(name + ' , class number: ' +  str(class_numb))
-
-        if(class_numb == count):
-            right_test_cases += 1
-        total_test_cases += 1
-
-        break
-    count += 1
-
-accuracy = (right_test_cases / total_test_cases) * 100
-print('Accuracy:  ' + str(accuracy) + '%')
 
 
 
