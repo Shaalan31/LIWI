da46876112ab28b14f028a5bff197eb2d2ff1151
Unknown <samar.gamal144@gmail.com>
2019-05-02 02:41:13 +0200

    Enhancing the code

---
+++ siftmodel/sift_model.py
@@ -0,0 +1,103 @@
+import cv2
+import pickle
+import glob
+from pathlib import Path
+from siftmodel.features import *
+from siftmodel.word_segmentation import *
+from siftmodel.feature_matching import *
+
+
+class SiftModel:
+    def __init__(self, first_class, last_class, code_book):
+        self.base_train = 'C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/Samples/'
+        self.base_test = 'C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/TestCases/'
+        self.first_class = first_class
+        self.last_class = last_class
+        self.code_book = code_book
+        # create needed objects
+        self.segmentation = WordSegmentation()
+        self.preprocess = Preprocessing()
+        self.features = FeaturesExtraction()
+
+    def tester(self, name, path):
+
+        image = cv2.imread(name)
+        image = self.preprocess.remove_shadow(image)
+
+        # extract handwriting from image
+        top, bottom = self.preprocess.extract_text(image)
+        image = image[top:bottom, :]
+        cv2.imwrite('image_extract_text.png', image)
+
+        # segment words and get its sift descriptors and orientations
+        sd, so = self.segmentation.word_segmentation(image, path)
+
+        # calculate SDS and SOH
+        SDS = self.features.sds(sd, self.code_book, t=1)
+        SOH = self.features.soh(so, phi=36)
+
+        return SDS, SOH
+
+    # Train the model
+    def train(self):
+
+        count = self.first_class
+        SDS_train = []
+        SOH_train = []
+        print('Training The Model:')
+
+        while count <= self.last_class:
+            for filename in glob.glob(self.base_train + 'Class' + str(count)):
+                print('Class' + str(count) + ':')
+
+                for image in glob.glob(filename + '/*.png'):
+                    name = Path(image).name
+                    print(name)
+
+                    SDS, SOH = self.tester(self.base_train + 'Class' + str(count) + '/' + name, name)
+                    SDS_train.append(SDS)
+                    SOH_train.append(SOH)
+
+                count += 1
+
+        return SDS_train, SOH_train
+
+    # Test the model
+    def test(self, SDS_train, SOH_train):
+
+        total_test_cases = 0
+        right_test_cases = 0
+        count = self.first_class
+        print('Testing The Model:')
+
+        matching = FeatureMatching()
+        while count <= self.last_class:
+            print('Class' + str(count) + ':')
+
+            for filename in glob.glob(self.base_test + 'testing' + str(count) + '_*.png'):
+                name = Path(filename).name
+
+                # self.tester('C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/TestCases/testing24_2.png', 'testing24_2.png')
+                SDS, SOH = self.tester(self.base_test + name, name)
+
+                distances = []
+                for i in range(0, len(SDS_train)):
+                    D = matching.match(u=SDS, v=SDS_train[i], x=SOH, y=SOH_train[i], w=0.35)
+                    distances.append(D)
+
+                class_numb = int(np.argmin(distances) / 2) + self.first_class
+                print(name + ' , class number: ' +  str(class_numb))
+
+                if(class_numb == count):
+                    right_test_cases += 1
+                total_test_cases += 1
+
+                accuracy = (right_test_cases / total_test_cases) * 100
+                print('Accuracy:  ' + str(accuracy) + '%')
+
+                # break
+            count += 1
+
+    def run(self):
+        SDS, SOH = self.train()
+        self.test(SDS_train=SDS, SOH_train=SOH)
