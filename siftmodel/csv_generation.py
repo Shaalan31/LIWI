afcf271bc71757160d68f63a5d49f263daaa4fc1
omar shaalan <omarshaalan31@gmail.com>
2019-05-02 23:07:24 +0200

    generating csv files for fast testing, no need for thank you

--- siftmodel/csv_generation.py
+++ siftmodel/csv_generation.py
@@ -4,8 +4,13 @@ from siftmodel.word_segmentation import *
 from siftmodel.feature_matching import *
 import numpy as np
 import pickle
+from pathlib import Path
+import glob
+import os, errno
 
-code_book = pickle.load( open( "siftmodel/centers.pkl", "rb" ) )
+
+
+code_book = pickle.load( open( "centers.pkl", "rb" ) )
 base_train = 'C:/Users/omars/Documents/Github/LIWI/Omar/Samples/'
 base_test = 'C:/Users/omars/Documents/Github/LIWI/Omar/TestCases/'
 base_train_SDS = 'C:/Users/omars/Documents/Github/LIWI/Omar/Fast/Samples/SDS/'
@@ -34,5 +39,41 @@ def get_features(name, path):
 
     return SDS, SOH
 
-for x  in range (1,159):
-    for
+
+def generate_train():
+    for count  in range (1,159):
+        for filename in glob.glob(base_train + 'Class' + str(count)):
+            print('Class' + str(count) + ':')
+
+            for image in glob.glob(filename + '/*.png'):
+                name = Path(image).name
+                print(name)
+
+                SDS, SOH = get_features(base_train + 'Class' + str(count) + '/' + name, name)
+                name = name.replace('png','csv')
+                print(SDS.shape)
+                print(SOH.shape)
+
+
+
+                np.savetxt(base_train_SDS +"Class"+str(count)+"/"+ name, SDS, delimiter=",")
+                np.savetxt(base_train_SOH +"Class"+str(count)+"/"+ name, SOH, delimiter=",")
+
+
+
+
+def generate_test():
+    for count in range (1,159):
+        print('Class' + str(count) + ':')
+
+        for filename in glob.glob(base_test + 'testing' + str(count) + '_*.png'):
+            name = Path(filename).name
+            SDS, SOH = get_features(base_test + name, name)
+
+            name = name.replace('png', 'csv')
+            np.savetxt(base_test_SDS + name, SDS, delimiter=",")
+            np.savetxt(base_test_SOH + name, SOH, delimiter=",")
+
+
+
+generate_test()
