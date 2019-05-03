e2368950daffc6c36008b7bb62bc1b5889070dbd
Unknown <samar.gamal144@gmail.com>
2019-05-03 22:51:14 +0200

    database connection

---
+++ server/dao/connection.py
@@ -0,0 +1,52 @@
+import pymongo
+
+
+class Database:
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.db = None
+        self.collection = None
+
+    def create_database(self):
+        databases = self.client.list_database_names()
+        print(databases)
+        if "LIWI" not in databases:
+            self.db = self.client["LIWI"]
+
+    def create_collection(self):
+        collections = self.db.list_collection_names()
+        if "writers" not in collections:
+            self.db.create_collection("writers")
+
+    def connect(self):
+        # get database
+        databases = self.client.list_database_names()
+        if "LIWI" in databases:
+            self.db = self.client["LIWI"]
+        else:
+            print('Database not created')
+
+        # get collection
+        collections = self.db.list_collection_names()
+        if "writers" in collections:
+            self.collection = self.db["writers"]
+        else:
+            print('Collection not created')
+
+    def get_client(self):
+        return self.client
+
+    def get_database(self):
+        return self.db
+
+    def get_collection(self):
+        return self.collection
+
+
+# db = Database()
+# db.create_database()
+# db.create_collection()
+# db.connect()
+# print(db.get_client())
+# print(db.get_database())
+# print(db.get_collection())
