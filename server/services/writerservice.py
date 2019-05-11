5b288efbfeecb750e053103a3de092312e250630
May Abdeldayem <may.ahmed1@hotmail.com>
2019-05-11 22:24:43 +0200

    Arabic api done

--- server/services/writerservice.py
+++ server/services/writerservice.py
@@ -4,7 +4,9 @@ from server.httpresponses.messages import HttpMessages
 from texturemodel.texture_model import *
 from horestmodel.horest_model import *
 from siftmodel.sift_model import *
+
 from server.views.writervo import *
+from server.utils.utilities import *
 
 from multiprocessing import Pool
 
@@ -13,7 +15,7 @@ texture_model = TextureWriterIdentification()
 sift_model = SiftModel()
 
 
-def predict_writer(testing_image, filename, writers_ids, dao,url):
+def predict_writer(testing_image, filename, writers_ids, dao, url):
     """
         Service to get predicted English writers
         :parameter: request contains
@@ -128,7 +130,8 @@ def predict_writer(testing_image, filename, writers_ids, dao,url):
     writers_predicted = []
     vfunc = np.vectorize(func)
     writer_predicted = writers[np.where(vfunc(writers) == final_prediction)[0][0]]
-    writer_vo = WriterVo(writer_predicted.id, writer_predicted.name, writer_predicted.username,url+writer_predicted.image)
+    writer_vo = WriterVo(writer_predicted.id, writer_predicted.name, writer_predicted.username,
+                         url + writer_predicted.image)
     writers_predicted.append(writer_vo)
 
     predictions = np.unique(predictions)
@@ -136,13 +139,14 @@ def predict_writer(testing_image, filename, writers_ids, dao,url):
         for prediction in predictions:
             if prediction != final_prediction:
                 writer_predicted = writers[np.where(vfunc(writers) == prediction)[0][0]]
-                writer_vo = WriterVo(writer_predicted.id, writer_predicted.name, writer_predicted.username,url+writer_predicted.image)
+                writer_vo = WriterVo(writer_predicted.id, writer_predicted.name, writer_predicted.username,
+                                     url + writer_predicted.image)
                 writers_predicted.append(writer_vo)
 
     return HttpErrors.SUCCESS, HttpMessages.SUCCESS, writers_predicted
 
 
-def predict_writer_arabic(testing_image, filename, writers_ids, dao,url):
+def predict_writer_arabic(testing_image, filename, writers_ids, dao, url):
     """
          Service to get predicted Arabic writers
          :parameter: request contains
@@ -230,7 +234,8 @@ def predict_writer_arabic(testing_image, filename, writers_ids, dao,url):
     writers_predicted = []
     vfunc = np.vectorize(func)
     writer_predicted = writers[np.where(vfunc(writers) == final_prediction)[0][0]]
-    writer_vo = WriterVo(writer_predicted.id, writer_predicted.name, writer_predicted.username,url+writer_predicted.image)
+    writer_vo = WriterVo(writer_predicted.id, writer_predicted.name, writer_predicted.username,
+                         url + writer_predicted.image)
     writers_predicted.append(writer_vo)
 
     predictions = np.unique(predictions)
@@ -238,7 +243,8 @@ def predict_writer_arabic(testing_image, filename, writers_ids, dao,url):
         for prediction in predictions:
             if prediction != final_prediction:
                 writer_predicted = writers[np.where(vfunc(writers) == prediction)[0][0]]
-                writer_vo = WriterVo(writer_predicted.id, writer_predicted.name, writer_predicted.username,url+writer_predicted.image)
+                writer_vo = WriterVo(writer_predicted.id, writer_predicted.name, writer_predicted.username,
+                                     url + writer_predicted.image)
                 writers_predicted.append(writer_vo)
 
     return HttpErrors.SUCCESS, HttpMessages.OK, writers_predicted
@@ -343,3 +349,147 @@ def func(writer):
     :return: attribute id
     """
     return writer.id
+
+
+def fill_collection(start_class, end_class, base_path, dao):
+    """
+        Service to fill English writers collection
+        :parameter: request contains
+                  - start class: start_class
+                  - end class: end_class
+                  - base path: base_path
+                  - dao: dao
+        :return:
+                -status
+                -message
+      """
+    num_classes = end_class
+
+    names, birthdays, phones, addresses, nid, images = fake_data()
+
+    # loop on the writers
+    for class_number in range(start_class, num_classes + 1):
+        writer_name = names[class_number - 1]
+
+        writer_horest_features = []
+        writer_texture_features = []
+        SDS_train = []
+        SOH_train = []
+        horest_model.num_lines_per_class = 0
+        texture_model.num_blocks_per_class = 0
+        print('Class' + str(class_number) + ':')
+
+        # loop on training data for each writer
+        for filename in glob.glob(
+                base_path + str(class_number) + '/*.jpg'):
+            print(filename)
+            image = cv2.imread(filename)
+            print('Horest Features')
+            # writer_horest_features.append(horest_model.get_features(cv2.imread(filename))[0].tolist())
+            _, horest_features = horest_model.get_features(image)
+            writer_horest_features = np.append(writer_horest_features, horest_features[0].tolist())
+            print('Texture Features')
+            # writer_texture_features.append(texture_model.get_features(cv2.imread(filename))[0].tolist())
+            _, texture_features = texture_model.get_features(image)
+            writer_texture_features = np.append(writer_texture_features, texture_features[0].tolist())
+            print('Sift Model')
+            name = Path(filename).name
+            SDS, SOH = sift_model.get_features(name, image=image)
+            SDS_train.append(SDS[0].tolist())
+            SOH_train.append(SOH[0].tolist())
+
+        writer_horest_features = horest_model.adjust_nan_values(
+            np.reshape(writer_horest_features,
+                       (horest_model.num_lines_per_class, horest_model.get_num_features()))).tolist()
+        writer_texture_features = texture_model.adjust_nan_values(
+            np.reshape(writer_texture_features,
+                       (texture_model.num_blocks_per_class, texture_model.get_num_features()))).tolist()
+
+        writer = Writer()
+        features = Features()
+        features.horest_features = writer_horest_features
+        features.texture_feature = writer_texture_features
+        features.sift_SDS = SDS_train
+        features.sift_SOH = SOH_train
+
+        writer.features = features
+        writer.id = class_number
+        writer.name = writer_name
+        writer.birthday = birthdays[class_number - 1]
+        writer.address = addresses[class_number - 1]
+        writer.phone = phones[class_number - 1]
+        writer.nid = nid[class_number - 1]
+        writer.image = images[class_number - 1]
+        name_splitted = writer.name.split()
+        writer.username = name_splitted[0][0].lower() + name_splitted[1].lower() + str(writer.id)
+        status_code, message = dao.create_writer(writer)
+        print(message.value)
+    return status_code, message
+
+
+def fill_collection_arabic(start_class, end_class, base_path, dao):
+    """
+        Service to fill Arabic writers collection
+        :parameter: request contains
+                  - start class: start_class
+                  - end class: end_class
+                  - base path: base_path
+                  - dao: dao
+        :return:
+                -status
+                -message
+    """
+    num_classes = end_class
+
+    names, birthdays, phones, addresses, nid, images = fake_data()
+
+    # loop on the writers
+    for class_number in range(start_class, num_classes + 1):
+        writer_name = names[class_number - 1]
+
+        writer_texture_features = []
+        # SDS_train = []
+        # SOH_train = []
+        horest_model.num_lines_per_class = 0
+        texture_model.num_blocks_per_class = 0
+        print('Class' + str(class_number) + ':')
+
+        # loop on training data for each writer
+        for filename in glob.glob(
+                base_path + str(class_number) + '/*.jpg'):
+            print(filename)
+            image = cv2.imread(filename)
+
+            print('Texture Features')
+            _, texture_features = texture_model.get_features(image)
+            writer_texture_features = np.append(writer_texture_features, texture_features[0].tolist())
+
+            # print('Sift Model')
+            # name = Path(filename).name
+            # SDS, SOH = sift_model.get_features(name, image=image)
+            # SDS_train.append(SDS[0].tolist())
+            # SOH_train.append(SOH[0].tolist())
+
+        writer_texture_features = texture_model.adjust_nan_values(
+            np.reshape(writer_texture_features,
+                       (texture_model.num_blocks_per_class, texture_model.get_num_features()))).tolist()
+
+        writer = Writer()
+        features = Features()
+        features.texture_feature = writer_texture_features
+        # features.sift_SDS = SDS_train
+        # features.sift_SOH = SOH_train
+
+        writer.features = features
+        writer.id = class_number
+        writer.name = writer_name
+        writer.birthday = birthdays[class_number - 1]
+        writer.address = addresses[class_number - 1]
+        writer.phone = phones[class_number - 1]
+        writer.nid = nid[class_number - 1]
+        writer.image = images[class_number - 1]
+        name_splitted = writer.name.split()
+        writer.username = name_splitted[0][0].lower() + name_splitted[1].lower() + str(writer.id)
+        status_code, message = dao.create_writer(writer)
+        print(message.value)
+    return status_code, message
