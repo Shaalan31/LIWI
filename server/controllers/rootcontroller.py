7db89228530cfd6c2bee3faa8e8cdaab70e5f321
Unknown <samar.gamal144@gmail.com>
2019-05-11 23:14:43 +0200

    add comments

--- server/controllers/rootcontroller.py
+++ server/controllers/rootcontroller.py
@@ -39,6 +39,9 @@ def handle_invalid_usage(error):
 def get_writers():
     """
     API to get all writers
+    :parameter: Query parameter lang
+                  - en for english
+                  - ar for arabic
     :raise: Exception containing:
             message:
             - "OK" for success
@@ -64,6 +67,9 @@ def get_writers():
 def get_prediction():
     """
       API for predicting a writer of the image
+      :parameter: Query parameter lang
+                  - en for english
+                  - ar for arabic
       :parameter: request contains
                   - writers ids: writers_ids
                   - image name: _filename
@@ -102,6 +108,9 @@ def get_prediction():
 def create_writer():
     """
     API for creating a new writer
+    :parameter: Query parameter lang
+                  - en for english
+                  - ar for arabic
     :parameter: request contains
                 - writer name: _name
                 - writer username: _username
@@ -143,7 +152,10 @@ def create_writer():
 def get_profile():
     """
     API to get writer's profile
-    :parameter: query parameter id
+    :parameter: Query parameter id
+                Query parameter lang
+                  - en for english
+                  - ar for arabic
     :raise: Exception containing:
             message:
             - "OK" for success
@@ -217,6 +229,9 @@ def get_image(path, filename):
 def update_writer_features():
     """
     API for updating a writer features
+    :parameter: Query parameter lang
+                  - en for english
+                  - ar for arabic
     :parameter: request contains
                 - image name: _filename
                 - writer id: _id
@@ -251,21 +266,24 @@ def update_writer_features():
 def set_writers():
     """
        API for filling database collection with dummy data
+       :parameter Query parameter lang
+                  - en for english
+                  - ar for arabic
        :raise: Exception contains
                - response message:
                    "OK" for success
                - response status code:
                    200 for success
        """
-    start_class = 1
+    start_class = 39
     end_class = 100
     language = request.args.get('lang', None)
     if language == "ar":
-        base_path = 'D:/Uni/Graduation Project/All Test Cases/KHATT/Samples/Class'
+        base_path = 'C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/KHATT/Samples/Class'
         status_code, message = fill_collection_arabic(start_class, end_class, base_path,
                                                       Writers(db.get_collection_arabic()))
     else:
-        base_path = 'D:/Uni/Graduation Project/All Test Cases/IAMJPG/Samples/Class'
+        base_path = 'C:/Users/Samar Gamal/Documents/CCE/Faculty/Senior-2/2st term/GP/writer identification/LIWI/TestCasesCompressed/Samples/Class'
         status_code, message = fill_collection(start_class, end_class, base_path, writers_dao)
 
     raise ExceptionHandler(message=message.value, status_code=status_code.value)
